5d6a1beb0c06b07141aa986e1e79be84ed6d6664
Vadim <vad7@yahoo.com>
2020-02-08 19:05:09 +0300

    fix

--- WaterTreatment/WaterTreatment.ino
+++ WaterTreatment/WaterTreatment.ino
@@ -776,7 +776,7 @@ xSetupExit:
 					setup = 0;
 				} else if((setup & 0xFF00) == 0x100) {	// menu item 1 selected - Relay
 					MC.dRelay[setup & 0xFF].set_Relay(MC.dRelay[setup & 0xFF].get_Relay() ? -fR_StatusManual : fR_StatusManual);
-				} else setup = (setup << 8) & LCD_SetupFlag; // select menu item
+				} else setup = (setup << 8) | LCD_SetupFlag; // select menu item
 				DisplayTick = ~DisplayTick;
 			} else if(MC.get_errcode() && !Error_Beep_confirmed) Error_Beep_confirmed = true; // Supress beeping
 			else { // Enter Setup
