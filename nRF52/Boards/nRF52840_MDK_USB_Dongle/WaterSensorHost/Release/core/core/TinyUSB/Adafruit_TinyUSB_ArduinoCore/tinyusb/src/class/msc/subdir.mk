d95c2992a9eab3fd4953d6a538ad49f0196429a2
Vadim <vad7@yahoo.com>
2020-03-06 16:41:23 +0300

    MDK board start

---
+++ nRF52/Boards/nRF52840_MDK_USB_Dongle/WaterSensorHost/Release/core/core/TinyUSB/Adafruit_TinyUSB_ArduinoCore/tinyusb/src/class/msc/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5\TinyUSB\Adafruit_TinyUSB_ArduinoCore\tinyusb\src\class\msc\msc_device.c 
+
+C_DEPS += \
+.\core\core\TinyUSB\Adafruit_TinyUSB_ArduinoCore\tinyusb\src\class\msc\msc_device.c.d 
+
+AR_OBJ += \
+.\core\core\TinyUSB\Adafruit_TinyUSB_ArduinoCore\tinyusb\src\class\msc\msc_device.c.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+core\core\TinyUSB\Adafruit_TinyUSB_ArduinoCore\tinyusb\src\class\msc\msc_device.c.o: C:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5\TinyUSB\Adafruit_TinyUSB_ArduinoCore\tinyusb\src\class\msc\msc_device.c
+	@echo 'Building file: $<'
+	@echo 'Starting C compile'
+	"C:\eclipse\arduinoPlugin\packages\arduino\tools\arm-none-eabi-gcc\7-2017q4/bin/arm-none-eabi-gcc" -mcpu=cortex-m4 -mthumb -c -g -Wall -Wextra -Wno-unused-parameter -Wno-missing-field-initializers -Wno-pointer-arith -mfloat-abi=hard -mfpu=fpv4-sp-d16 -u _printf_float -std=gnu11 -ffunction-sections -fdata-sections -nostdlib --param max-inline-insns-single=500 -MMD -DF_CPU=64000000 -DARDUINO=10812 -DARDUINO_NRF52840_FEATHER -DARDUINO_ARCH_NRF52 "-DARDUINO_BSP_VERSION=\"0.18.5\""  -DNRF52840_XXAA -DUSBCON -DUSE_TINYUSB -DUSB_VID=0x239A -DUSB_PID=0x8029 '-DUSB_MANUFACTURER=\"Adafruit LLC\"' '-DUSB_PRODUCT=\"Feather nRF52840 Express\"' -DSOFTDEVICE_PRESENT -DARDUINO_NRF52_ADAFRUIT -DNRF52_SERIES -DLFS_NAME_MAX=64 -Ofast -DCFG_DEBUG=0 "-IC:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5/cmsis/include" "-IC:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5/nordic" "-IC:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5/nordic/nrfx" "-IC:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5/nordic/nrfx/hal" "-IC:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5/nordic/nrfx/mdk" "-IC:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5/nordic/nrfx/soc" "-IC:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5/nordic/nrfx/drivers/include" "-IC:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5/nordic/nrfx/drivers/src" "-IC:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5/nordic/softdevice/s140_nrf52_6.1.1_API/include" "-IC:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5/freertos/Source/include" "-IC:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5/freertos/config" "-IC:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5/freertos/portable/GCC/nrf52" "-IC:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5/freertos/portable/CMSIS/nrf52" "-IC:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5/sysview/SEGGER" "-IC:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5/sysview/Config" "-IC:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5/TinyUSB" "-IC:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5/TinyUSB/Adafruit_TinyUSB_ArduinoCore" "-IC:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5/TinyUSB/Adafruit_TinyUSB_ArduinoCore/tinyusb/src"   -I"C:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5\utility" -I"C:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\cores\nRF5" -I"C:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\variants\feather_nrf52840_express" -I"C:\eclipse\arduinoPlugin\packages\adafruit\hardware\nrf52\0.18.5\libraries\SPI" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -D__IN_ECLIPSE__=1 "$<"  -o  "$@"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ nRF52/Boards/nRF52840_MDK_USB_Dongle/WaterSensorHost/WaterSensorHost.ino
@@ -0,0 +1,167 @@
+#include "Arduino.h"
+#define NRF5
+#define NRF52840
+#include "nrf.h"
+#include "nrf_radio.h"
+//#ifdef ARDUINO_ARCH_NRF52
+//#define ARDUINO_ARCH_NRF52
+//#endif
+#define MY_WAKE_UP_BY_TIMER			((int8_t)-1)		//!< Sleeping wake up by timer
+#define MY_SLEEP_NOT_POSSIBLE		((int8_t)-2)		//!< Sleeping not possible
+#define INTERRUPT_NOT_DEFINED		((uint8_t)255)	//!< _sleep() param: no interrupt defined
+#define MODE_NOT_DEFINED				((uint8_t)255)	//!< _sleep() param: no mode defined
+#define VALUE_NOT_DEFINED				((uint8_t)255)	//!< Value not defined
+#define FUNCTION_NOT_SUPPORTED	((uint16_t)0)		//!< Function not supported
+
+#include "NRF5\MyConfig.h"
+#include "NRF5\MyHwHAL.h"
+#include "NRF5\Radio.h"
+#include "NRF5\Radio.cpp"
+//#include "NRF5\MyHwHAL.cpp"
+#include "NRF5\MyHwNRF5.h"
+#include "NRF5\MyHwNRF5.cpp"
+#include "NRF5\Radio_ESB.h"
+//#include "NRF5\Radio_ESB.cpp"
+#include "NRF5\MyTransportNRF5_ESB.cpp"
+#include "NRF5\MyMainNRF5.cpp"
+
+
+int main(void)
+{
+	init();
+#if defined(USBCON)
+	USBDevice.attach();
+#endif
+	_begin(); // Startup MySensors library
+	for(;;) {
+		_process();  // Process incoming data
+		if (loop) {
+			loop(); // Call sketch loop
+		}
+		if (serialEventRun) {
+			serialEventRun();
+		}
+	}
+	return 0;
+}
+
+
+
+/*
+#include <MySensors.h>
+//#define NRF52
+#define MY_LED 0
+#define MY_KEY 4
+//#define MY_BUTTON_ADC_PIN X // номер пина АЦП смотреть в MyBoardNRF5.cpp
+//#define MY_BUTTON_PIN (MY_BUTTON_ADC_PIN X-1)  //this is the same pin number MY_BUTTON_ADC_PIN, see the pins table above (only nrf51, I think so)
+#define MY_BUTTON_ADC_PIN 6 // номер пина АЦП смотреть в MyBoardNRF5.cpp
+//#define MY_BUTTON_PIN 5  //this is the same pin number MY_BUTTON_ADC_PIN, see the pins table above (only nrf51, I think so)
+
+
+void preHwInit()
+{
+	pinMode(MY_LED, OUTPUT);
+	//  pinMode(MY_BUTTON_PIN, INPUT_PULLUP);
+	pinMode(MY_KEY, INPUT_PULLUP);
+	digitalWrite(MY_LED, LOW);
+}
+
+void before()
+{
+	NRF_POWER->DCDCEN = 1; // закоментировать если E73
+	NRF_UART0->ENABLE = 0;
+}
+
+void setup()
+{
+	analogReference (AR_VBG);
+	send(msgVolt.set(((float) hwCPUVoltage() / 1000), 2)); //отправляю напряжение батареи
+	lpComp();  // if y use lpcomp interrupt
+	led(4, 1);
+	detection = 0;
+	wait(1000);
+}
+
+void loop()
+{
+	sleep (sleep_batt);
+	if(detection == 1) {
+		s_lpComp();
+		if(digitalRead(MY_KEY) == 1) {
+			send(msgDoor.setDestination(0).set("true!"));
+			led(2, 1);
+			wait(100);
+		}
+		if(digitalRead(MY_KEY) == 0) {
+			send(msgDoor.setDestination(0).set("false!"));
+			led(1, 1);
+			wait(100);
+		}
+		//    led(3, 3);
+		detection = 0;
+		NRF_LPCOMP->EVENTS_CROSS = 0;
+		r_lpComp();
+	} else {
+		send(msgVolt.set(((float) hwCPUVoltage() / 1000), 2)); //отправляю напряжение батареи
+	}
+}
+
+void led(uint8_t flash, uint8_t iteration)
+{
+	//    for (int x = 0; x < iteration; x++) {  //повтор
+	for(int i = 0; i < flash; i++) {
+		digitalWrite(MY_LED, HIGH);
+		wait(20);
+		digitalWrite(MY_LED, LOW);
+		wait(20);
+		//    }
+		//    wait(500);
+	}
+}
+
+void lpComp()
+{
+	NRF_LPCOMP->PSEL = MY_BUTTON_ADC_PIN;
+	NRF_LPCOMP->ANADETECT = 0; // детектирование EVENTS_DOWN.
+	//ANADETECT=0; // детектирование EVENTS_CROSS.
+	//ANADETECT=1; // детектирование EVENTS_UP.
+	//ANADETECT=2; // детектирование EVENTS_DOWN.
+	NRF_LPCOMP->INTENSET = B1000; // активация прерывания для EVENTS_DOWN
+	//B1000; // прерываниt для EVENTS_CROSS
+	//B0100; // прерываниt для EVENTS_UP
+	//B1000; // прерываниt для EVENTS_DOWN
+	NRF_LPCOMP->ENABLE = 1;
+	NRF_LPCOMP->TASKS_START = 1;
+	NVIC_SetPriority(LPCOMP_IRQn, 15);
+	NVIC_ClearPendingIRQ(LPCOMP_IRQn);
+	NVIC_EnableIRQ(LPCOMP_IRQn);
+}
+
+void s_lpComp()
+{
+	if((NRF_LPCOMP->ENABLE) && (NRF_LPCOMP->EVENTS_READY)) {
+		NRF_LPCOMP->INTENCLR = B1000; //деактивация прерывания для LPCOMP
+	}
+}
+
+void r_lpComp()
+{
+	NRF_LPCOMP->INTENSET = B1000;
+}
+#if __CORTEX_M == 0x04
+#define NRF5_RESET_EVENT(event)                                                 \
+		event = 0;                                                                   \
+		(void)event
+#else
+#define NRF5_RESET_EVENT(event) event = 0
+#endif
+extern "C" {
+	void LPCOMP_IRQHandler(void)
+	{
+		detection = 1;
+		NRF5_RESET_EVENT(NRF_LPCOMP->EVENTS_CROSS);
+		NRF_LPCOMP->EVENTS_CROSS = 0;
+		MY_HW_RTC->CC[0] = (MY_HW_RTC->COUNTER + 2);
+	}
+}
+*/
