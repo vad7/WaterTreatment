4533e61c03c81b4e13d5541ac62fafd401d41c95
vad7 <vad7@yahoo.com>
2025-08-09 14:53:36 +0300

    upd

--- WaterTreatment/devOneWire.ino
+++ WaterTreatment/devOneWire.ino
@@ -315,65 +315,66 @@ void Recover_I2C_bus(void)
 {
 	pinMode(PIN_WIRE_SDA, INPUT);
 	delayMicroseconds(20);
-	if(digitalReadDirect(PIN_WIRE_SDA)) return;
-
-	TWI0->TWI_CR = TWI_CR_SVDIS | TWI_CR_MSDIS;
-	pmc_disable_periph_clk(WIRE_INTERFACE_ID);// Отключить I2C периферию
-//	PMC->PMC_PCER0 = PMC_PCER0_PID12;  // PIOB power ON
-//	PIOB->PIO_PER |= PIO_PER_P12;      // TWCK1 pin (SCL) back to GPIO
-//	PIOB->PIO_OER |= PIO_OER_P12;
-//	PIOB->PIO_OWER |= PIO_OWER_P12;
-//	PIOB->PIO_PER |= PIO_PER_P13;      // TWD1 pin (SDA)  back to GPIO
-//	PIOB->PIO_OER |= PIO_OER_P13;
-//	PIOB->PIO_OWER |= PIO_OWER_P13;
-//  I2C#2 (TWI1)
-//  TWI1->TWI_CR = TWI_CR_SVDIS | TWI_CR_MSDIS;
-//	PMC->PMC_PCER0 = PMC_PCER0_PID11;  // PIOA power ON
-//	PIOA->PIO_PER |= PIO_PER_P18;      // TWCK0 pin (SCL1) back to GPIO
-//	PIOA->PIO_OER |= PIO_OER_P18;
-//	PIOA->PIO_OWER |= PIO_OWER_P18;
-//	PIOA->PIO_PER |= PIO_PER_P17;      // TWD0 pin (SDA1)  back to GPIO
-//	PIOA->PIO_OER |= PIO_OER_P17;
-//	PIOA->PIO_OWER |= PIO_OWER_P17;
-	//pinMode(PIN_WIRE_SDA, OUTPUT);// Перевести пины в GPIO режим
-	//pinMode(PIN_WIRE_SCL, OUTPUT);// Перевести пины в GPIO режим
-	// TWI0:
-//#define I2C_PORT		PIOA
-//#define I2C_SDA	 	PIO_PB17
-//#define I2C_SCL	 	PIO_PB18
-	// TWI1:
-#define I2C_PORT	PIOB
-#define I2C_SDA	 	PIO_PB12
-#define I2C_SCL	 	PIO_PB13
-	I2C_PORT->PIO_PER = I2C_SDA | I2C_SCL;  // Включить управление через PIO
-	I2C_PORT->PIO_MDER = I2C_SDA | I2C_SCL; // Режим Open-Drain
-	I2C_PORT->PIO_OER = I2C_SDA | I2C_SCL;  // Режим выхода
-	I2C_PORT->PIO_CODR = I2C_SDA | I2C_SCL; // Установить LOW
-	delayMicroseconds(20);
-	// Generate 9 clock pulses
-	for (uint8_t i = 0; i < 9; i++) {
-		I2C_PORT->PIO_ODR = I2C_SCL;  // SCL: open-drain (Hi-Z)
+	if(!digitalReadDirect(PIN_WIRE_SDA)) { // SDA зависла
+		TWI0->TWI_CR = TWI_CR_SVDIS | TWI_CR_MSDIS;
+		pmc_disable_periph_clk(WIRE_INTERFACE_ID);// Отключить I2C периферию
+	//	PMC->PMC_PCER0 = PMC_PCER0_PID12;  // PIOB power ON
+	//	PIOB->PIO_PER |= PIO_PER_P12;      // TWCK1 pin (SCL) back to GPIO
+	//	PIOB->PIO_OER |= PIO_OER_P12;
+	//	PIOB->PIO_OWER |= PIO_OWER_P12;
+	//	PIOB->PIO_PER |= PIO_PER_P13;      // TWD1 pin (SDA)  back to GPIO
+	//	PIOB->PIO_OER |= PIO_OER_P13;
+	//	PIOB->PIO_OWER |= PIO_OWER_P13;
+	//  I2C#2 (TWI1)
+	//  TWI1->TWI_CR = TWI_CR_SVDIS | TWI_CR_MSDIS;
+	//	PMC->PMC_PCER0 = PMC_PCER0_PID11;  // PIOA power ON
+	//	PIOA->PIO_PER |= PIO_PER_P18;      // TWCK0 pin (SCL1) back to GPIO
+	//	PIOA->PIO_OER |= PIO_OER_P18;
+	//	PIOA->PIO_OWER |= PIO_OWER_P18;
+	//	PIOA->PIO_PER |= PIO_PER_P17;      // TWD0 pin (SDA1)  back to GPIO
+	//	PIOA->PIO_OER |= PIO_OER_P17;
+	//	PIOA->PIO_OWER |= PIO_OWER_P17;
+		//pinMode(PIN_WIRE_SDA, OUTPUT);// Перевести пины в GPIO режим
+		//pinMode(PIN_WIRE_SCL, OUTPUT);// Перевести пины в GPIO режим
+		// TWI0:
+	//#define I2C_PORT		PIOA
+	//#define I2C_SDA	 	PIO_PB17
+	//#define I2C_SCL	 	PIO_PB18
+		// TWI1:
+	#define I2C_PORT	PIOB
+	#define I2C_SDA	 	PIO_PB12
+	#define I2C_SCL	 	PIO_PB13
+		I2C_PORT->PIO_PER = I2C_SDA | I2C_SCL;  // Включить управление через PIO
+		I2C_PORT->PIO_MDER = I2C_SDA | I2C_SCL; // Режим Open-Drain
+		I2C_PORT->PIO_OER = I2C_SDA | I2C_SCL;  // Режим выхода
+		I2C_PORT->PIO_CODR = I2C_SDA | I2C_SCL; // Установить LOW
 		delayMicroseconds(20);
-	    I2C_PORT->PIO_OER = I2C_SCL;   // Включить режим вывода
-	    I2C_PORT->PIO_CODR = I2C_SCL;  // SCL=0
+		// Generate 9 clock pulses
+		for (uint8_t i = 0; i < 9; i++) {
+			I2C_PORT->PIO_ODR = I2C_SCL;  // SCL: open-drain (Hi-Z)
+			delayMicroseconds(20);
+			I2C_PORT->PIO_OER = I2C_SCL;   // Включить режим вывода
+			I2C_PORT->PIO_CODR = I2C_SCL;  // SCL=0
+			delayMicroseconds(20);
+			if(digitalReadDirect(PIN_WIRE_SDA) == HIGH) break;  // Проверка освобождения SDA
+		}
+		// Форсированный STOP сигнал
+		I2C_PORT->PIO_CODR = I2C_SDA;  // SDA=0
+		I2C_PORT->PIO_OER = I2C_SDA;   // Включить вывод SDA
 		delayMicroseconds(20);
-		if(digitalReadDirect(PIN_WIRE_SDA) == HIGH) break;  // Проверка освобождения SDA
+		I2C_PORT->PIO_ODR = I2C_SCL;  // SCL=Hi-Z (внешняя подтяжка поднимет)
+		delayMicroseconds(20);
+		I2C_PORT->PIO_ODR = I2C_SDA;  // SDA=Hi-Z (STOP условие)
+		pinMode(PIN_WIRE_SDA, INPUT);
+		delayMicroseconds(20);
+	//	if(!digitalReadDirect(PIN_WIRE_SDA) && _reset) repower();
+		journal.printf("I2C Resetted\n");
+		PIO_Configure(
+				g_APinDescription[PIN_WIRE_SCL].pPort,
+				g_APinDescription[PIN_WIRE_SCL].ulPinType,
+				g_APinDescription[PIN_WIRE_SCL].ulPin,
+				g_APinDescription[PIN_WIRE_SCL].ulPinConfiguration);
 	}
-	// Форсированный STOP сигнал
-	I2C_PORT->PIO_CODR = I2C_SDA;  // SDA=0
-	I2C_PORT->PIO_OER = I2C_SDA;   // Включить вывод SDA
-	delayMicroseconds(20);
-	I2C_PORT->PIO_ODR = I2C_SCL;  // SCL=Hi-Z (внешняя подтяжка поднимет)
-	delayMicroseconds(20);
-	I2C_PORT->PIO_ODR = I2C_SDA;  // SDA=Hi-Z (STOP условие)
-	pinMode(PIN_WIRE_SDA, INPUT);
-	delayMicroseconds(20);
-//	if(!digitalReadDirect(PIN_WIRE_SDA) && _reset) repower();
-	PIO_Configure(
-			g_APinDescription[PIN_WIRE_SCL].pPort,
-			g_APinDescription[PIN_WIRE_SCL].ulPinType,
-			g_APinDescription[PIN_WIRE_SCL].ulPin,
-			g_APinDescription[PIN_WIRE_SCL].ulPinConfiguration);
 	PIO_Configure(
 			g_APinDescription[PIN_WIRE_SDA].pPort,
 			g_APinDescription[PIN_WIRE_SDA].ulPinType,
@@ -385,5 +386,4 @@ void Recover_I2C_bus(void)
 	Wire.begin();
 	Wire.setClock(I2C_SPEED);
 	delay(1);
-	journal.printf("I2C Resetted\n");
 }
